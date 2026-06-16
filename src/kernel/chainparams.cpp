a3b557789e14d6e1c4de14fdc80e1973da637426
Ali Kutlusoy <ali@kutlusoy.com>
2026-06-16 19:51:06 +0200

    Update chainparams.cpp

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -131,8 +131,9 @@ public:
         // This is fine at runtime as we'll fall back to using them as an addrfetch if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed.elektron-net.org.");
         vSeeds.emplace_back("seed0.eleknet.org.");   // operated by kutlusoy
+        vSeeds.emplace_back("seed.elektron-net.org");
+        
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,0);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
